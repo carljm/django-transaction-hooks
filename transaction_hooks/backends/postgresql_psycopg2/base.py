ff4bc8709733e3880d09f4ec9aac166dca2af148
Carl Meyer <carl@oddbird.net>
2014-01-17 23:41:28 -0700

    Move implementation into a mixin so it can be used with other backends.

--- transaction_hooks/backends/postgresql_psycopg2/base.py
+++ transaction_hooks/backends/postgresql_psycopg2/base.py
@@ -1,58 +1,8 @@
-"""A PostgreSQL Django backend with hooks for transaction-deferred execution."""
-
 from django.db.backends.postgresql_psycopg2 import base
 
-
-class DatabaseWrapper(base.DatabaseWrapper):
-    def __init__(self, *a, **kw):
-        super(DatabaseWrapper, self).__init__(*a, **kw)
-
-        # a list of no-argument functions to run when the transaction commits;
-        # each entry is an (sids, func) tuple, where sids is a list of the
-        # active savepoint IDs when this function was registered
-        self.run_on_commit = []
+from transaction_hooks.mixin import TransactionHooksDatabaseWrapperMixin
 
 
-    def on_commit(self, func):
-        if self.in_atomic_block:
-            # transaction in progress; save for execution on commit
-            self.run_on_commit.append((self.savepoint_ids[:], func))
-        else:
-            # no transaction in progress; execute immediately
-            func()
-
-
-
-    def commit(self, *a, **kw):
-        super(DatabaseWrapper, self).commit(*a, **kw)
-
-        for sids, func in self.run_on_commit:
-            func()
-
-        self.run_on_commit = []
-
-
-    def savepoint_rollback(self, sid, *a, **kw):
-        super(DatabaseWrapper, self).savepoint_rollback(sid, *a, **kw)
-
-        # remove any callbacks registered while this savepoint was active
-        self.run_on_commit = list(filter(
-            lambda x: sid not in x[0], self.run_on_commit))
-
-
-    def rollback(self, *a, **kw):
-        super(DatabaseWrapper, self).connect(*a, **kw)
-
-        self.run_on_commit = []
-
-
-    def connect(self, *a, **kw):
-        super(DatabaseWrapper, self).connect(*a, **kw)
-
-        self.run_on_commit = []
-
-
-    def close(self, *a, **kw):
-        super(DatabaseWrapper, self).close(*a, **kw)
-
-        self.run_on_commit = []
+class DatabaseWrapper(TransactionHooksDatabaseWrapperMixin,
+                      base.DatabaseWrapper):
+    pass
---
+++ transaction_hooks/mixin.py
@@ -0,0 +1,65 @@
+class TransactionHooksDatabaseWrapperMixin(object):
+    """
+    A ``DatabaseWrapper`` mixin to implement transaction-committed hooks.
+
+    To use, create a package for your custom database backend and place a
+    ``base.py`` module within it. Import whatever ``DatabaseWrapper`` you want
+    to subclass (under some other name), and then create a ``DatabaseWrapper``
+    class which inherits from both this mixin and the parent
+    ``DatabaseWrapper`` (in that order).
+
+    For an example, see ``backends/postgresql_psycopg2/base.py``.
+
+    """
+    def __init__(self, *a, **kw):
+        super(TransactionHooksDatabaseWrapperMixin, self).__init__(*a, **kw)
+
+        # a list of no-argument functions to run when the transaction commits;
+        # each entry is an (sids, func) tuple, where sids is a list of the
+        # active savepoint IDs when this function was registered
+        self.run_on_commit = []
+
+
+    def on_commit(self, func):
+        if self.in_atomic_block:
+            # transaction in progress; save for execution on commit
+            self.run_on_commit.append((self.savepoint_ids[:], func))
+        else:
+            # no transaction in progress; execute immediately
+            func()
+
+
+
+    def commit(self, *a, **kw):
+        super(TransactionHooksDatabaseWrapperMixin, self).commit(*a, **kw)
+
+        for sids, func in self.run_on_commit:
+            func()
+
+        self.run_on_commit = []
+
+
+    def savepoint_rollback(self, sid, *a, **kw):
+        super(TransactionHooksDatabaseWrapperMixin, self).savepoint_rollback(sid, *a, **kw)
+
+        # remove any callbacks registered while this savepoint was active
+        self.run_on_commit = list(filter(
+            lambda x: sid not in x[0], self.run_on_commit))
+
+
+    def rollback(self, *a, **kw):
+        super(TransactionHooksDatabaseWrapperMixin, self).connect(*a, **kw)
+
+        self.run_on_commit = []
+
+
+    def connect(self, *a, **kw):
+        super(TransactionHooksDatabaseWrapperMixin, self).connect(*a, **kw)
+
+        self.run_on_commit = []
+
+
+    def close(self, *a, **kw):
+        super(TransactionHooksDatabaseWrapperMixin, self).close(*a, **kw)
+
+        self.run_on_commit = []
