100f21ea8759257349dedb17d1ba022fc9c63a05
Carl Meyer <carl@oddbird.net>
2014-01-17 23:34:03 -0700

    Make runtests.py properly report success/fail status to tox.

--- runtests.py
+++ runtests.py
@@ -2,8 +2,9 @@
 # This script exists so this dir is on sys.path when running pytest in tox.
 import pytest
 import os
+import sys
 
 os.environ.setdefault(
     'DJANGO_SETTINGS_MODULE', 'transaction_hooks.test.settings_pg')
 
-pytest.main()
+sys.exit(pytest.main())
